44ca745157148032ca8d6c24e52c468adae75b69
Source Academy User <Lyndon Purcell>
2022-08-20 16:43:08 +1000

    Changes made from Source Academy

--- exercise1_32.js
+++ exercise1_32.js
@@ -6,18 +6,30 @@ function inc(n)         { return n + 1; }
 function add(x, y)      { return x + y; }
 function multiply(x, y) { return x * y; }
 
-// Generalised accumulation function
-function accumulate(combiner, null_value, term, a, next, b) {
+// Generalised accumulation functions -- iterative and recursive
+function accumulate_i(combiner, null_value, term, a, next, b) {
+    function iter(count, result) {
+        return count > b
+               ? result
+               : iter(next(count), combiner(term(count), result));
+    }
+    return iter(a, null_value);
+}
+
+function accumulate_r(combiner, null_value, term, a, next, b) {
     return a > b
            ? null_value
-           : combiner(term(a), accumulate(combiner, null_value, term, next(a), next, b));
+           : combiner(term(a), accumulate_r(combiner, null_value, term, next(a), next, b));
 }
+
 
 // Sum and Product defined in terms of Accumulate
 function sum(term, a, next, b) {
-    return accumulate(add, 0, term, a, next, b);
+    return accumulate_r(add, 0, term, a, next, b);
 }
 
 function product(term, a, next, b) {
-    return accumulate(multiply, 1, term, a, next, b);
+    return accumulate_r(multiply, 1, term, a, next, b);
 }
+
+product(identity, 3, inc, 5);
