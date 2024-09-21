0ddf2770f538f05648e8f1cc9fffc292bfd95b57
Lyndon Purcell <No public email provided>
2024-09-21 15:41:34 +1000

    Exercise 1.11: Iterative solution

--- exercise1_11.js
+++ exercise1_11.js
@@ -1,11 +1,27 @@
 // Exercise 1.11
 
 // Recursive solution
-function f_recursive(n) {
-    return n < 3
+function f_rec(n) {
+    return n < 3 
            ? n
-           : f_r(n-1) + 2*f_r(n-2) + 3*f_r(n-3);
+           : f_rec(n-1) + 2 * f_rec(n-2) + 3 * f_rec(n-3);
 }
 
-
 // Iterative solution
+function f_iter(n) {
+    function f(one_back, two_back, three_back, count) {
+        return count === n + 1
+               ? one_back
+               : f(one_back + 2*two_back + 3*three_back, 
+                   one_back, 
+                   two_back, 
+                   count+1);
+               
+    }
+    
+    return n < 3
+           ? n
+           : f(2, 1, 0, 3);
+}
+
+f_rec(120);
